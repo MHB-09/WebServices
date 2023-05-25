4f0223afd87ea3974b070c2a267f675229e8ce7d
MHB-09 <78057439+MHB-09@users.noreply.github.com>
2023-05-25 17:53:50 +0000

    Add files via upload

---
+++ etudiants.js
@@ -0,0 +1,29 @@
+const etudiants = [{
+  "id": 1,
+  "nom": "CISSE",
+  "prenom": "Mohamed",
+  "age": "20"
+}, {
+  "id": 2,
+  "nom": "Fall",
+  "prenom": "Abdou",
+  "age": "35"
+}, {
+  "id": 3,
+  "nom": "KANE",
+  "prenom": "ISSA",
+  "age": "25"
+}, {
+  "id": 4,
+  "nom": "SENE",
+  "prenom": "Fallou",
+  "age": "28"
+}, {
+  "id": 5,
+  "nom": "Sagna",
+  "prenom": "Omar",
+  "age": "30"
+}, ]
+
+
+module.exports = etudiants 
